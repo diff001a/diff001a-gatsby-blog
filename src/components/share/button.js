d9d2181eb65c453fc9f18f4242a0c57aed1cc333
diff001a <diff001a@gmail.com>
2019-09-23 19:01:59 +0900

    [update] シェアボタンのデザイン変更

--- src/components/share/button.js
+++ src/components/share/button.js
@@ -4,18 +4,19 @@ import { rgba } from "polished"
 
 const Wrapper = styled.div`
   background: #cccccc;
-  width: calc(100% / 4 - 10px);
-  padding: 7px 0;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
   color: #fff;
   font-weight: 600;
-  border-radius: 5px;
   cursor: pointer;
   transition: 0.3s ease-in-out;
   font-size: 0.95rem;
   opacity: 0.8;
+  margin: 0 10px;
   &:last-child {
     margin-right: 0;
   }
@@ -29,41 +30,36 @@ const Wrapper = styled.div`
     height: 100%;
     svg,
     b {
-      margin-right: 10px;
-      font-size: 1.2rem;
+      font-size: 0.9rem;
     }
   }
   &.twitter {
     background: linear-gradient(0deg, #1da1f2, ${rgba("#1DA1F2", 0.7)});
     text-shadow: 0 1px 1px #1b98e4;
-    box-shadow: 0 3px 8px -1px rgba(0, 0, 0, 0.1),
-      0 3px 16px -1px ${rgba("#1DA1F2", 0.6)};
+    box-shadow: 0 2px 8px -1px rgba(0, 0, 0, 0.1),
+      0 2px 10px -1px ${rgba("#1DA1F2", 0.6)};
   }
   &.facebook {
     background: linear-gradient(0deg, #3c5a99, ${rgba("#3C5A99", 0.8)});
     text-shadow: 0 1px 1px #334d83;
-    box-shadow: 0 3px 8px -1px rgba(0, 0, 0, 0.1),
-      0 3px 16px -1px ${rgba("#3C5A99", 0.6)};
+    box-shadow: 0 2px 8px -1px rgba(0, 0, 0, 0.1),
+      0 2px 10px -1px ${rgba("#3C5A99", 0.6)};
   }
   &.hatena {
     background: linear-gradient(0deg, #00a4dd, ${rgba("#00A4DD", 0.8)});
     text-shadow: 0 1px 1px #018cbc;
-    box-shadow: 0 3px 8px -1px rgba(0, 0, 0, 0.1),
-      0 3px 16px -1px ${rgba("#00A4DD", 0.6)};
+    box-shadow: 0 2px 8px -1px rgba(0, 0, 0, 0.1),
+      0 2px 10px -1px ${rgba("#00A4DD", 0.6)};
   }
   &.pocket {
     background: linear-gradient(0deg, #ee4056, ${rgba("#ee4056", 0.7)});
     text-shadow: 0 1px 1px #d13b4e;
-    box-shadow: 0 3px 8px -1px rgba(0, 0, 0, 0.1),
-      0 3px 16px -1px ${rgba("#ee4056", 0.6)};
+    box-shadow: 0 2px 8px -1px rgba(0, 0, 0, 0.1),
+      0 2px 10px -1px ${rgba("#ee4056", 0.6)};
   }
   &:hover {
     opacity: 1;
-  }
-  @media screen and (max-width: 780px) {
-    width: calc(100% / 2 - 5px);
-    margin-bottom: 10px;
-    height: 38px;
+    box-shadow: none;
   }
 `
 
