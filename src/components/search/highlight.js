6d5ca6c3c6441a2f1d12e36d3154ad46bd9e04e2
diff001a <diff001a@gmail.com>
2019-09-27 05:13:02 +0900

    [update] refactor highlight

--- src/components/search/highlight.js
+++ src/components/search/highlight.js
@@ -1,15 +1,16 @@
 import React from "react"
-import styled from "styled-components"
-const Wrapper = styled.span`
-  span {
-    background: rgba(255, 250, 118, 0.4);
-    padding: 3px 0;
-  }
-`
+
 const TextHighlighter = props => {
   const { str, includes } = props
   const temp = str.toLowerCase()
-  let res = temp.replace(includes, `<span>${includes}</span>`)
-  return <Wrapper dangerouslySetInnerHTML={{ __html: res }} />
+  const start = temp.indexOf(includes)
+  const end = start + includes.length
+  const item = str.slice(start, end)
+  const res = str.replace(
+    item,
+    `<span style="background: rgba(255, 250, 118, 0.4); padding: 3px 0">${item}</span>`
+  )
+  return <span dangerouslySetInnerHTML={{ __html: res }} />
 }
+
 export default TextHighlighter
