24ffbccfb83242f279cbd4b6b614345ff4b65d01
diff001a <diff001a@gmail.com>
2019-10-14 03:21:21 +0900

    [update] 日付フォーマット統一

--- src/templates/tag-page/index.js
+++ src/templates/tag-page/index.js
@@ -67,7 +67,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "YYYY-MM-DD")
             title
             description
             tags
